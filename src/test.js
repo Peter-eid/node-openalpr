6437a7836f1042371b23b7c29cd83047ab9c02ea
Basile Mellac <bmellac@switch.tv>
2017-05-19 19:17:03 +1000

    - added timing - max iteration as var

--- src/test.js
+++ src/test.js
@@ -1,6 +1,8 @@
 var openalpr = require('./openalpr');
 var fs = require('fs');
- 
+
+
+const maxit = 50;
 function identify (id, buffer) {
     //let data = fs.readFileSync(path);
     console.log (openalpr.IdentifyLicense (buffer, { regions : [{x:346, y:206, width:1318, height:600}]}, function (error, output) {
@@ -8,16 +10,18 @@ function identify (id, buffer) {
     //console.log(JSON.stringify(output, null, 4))
     console.log (id +" "+ output.processing_time_ms +" "+ ((results.length > 0) ? results[0].plate : "No results"));
 
-    if (id == 248) {
-        console.log (openalpr.Stop ());
+    if (id == maxit) {
+        console.log(openalpr.Stop());
+        console.timeEnd('lpr');
     }
     }));
 }
  
-openalpr.Start ();
+openalpr.Start (null, null,4)
 openalpr.GetVersion ();
  
 const buffer = fs.readFileSync("10.125.23.3_01_20170321102708002_VEHICLE_DETECTION.jpg");
-for (var i = 0; i < 249; i++) {
+console.time('lpr');
+for (var i = 0; i <= maxit; i++) {
     identify (i, buffer);
 }
